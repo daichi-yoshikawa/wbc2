18d6c77f55fa41bf1267896d9b4cd9dd82ae9503
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2020-02-17 05:39:43 -0800

    Updated CMakeLists.txt.

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 
+#include "wbc2/version.hpp"
 #include "wbc2/robot/robot.hpp"
 #include "wbc2/controller/controller.hpp"
 
@@ -10,6 +11,7 @@ using namespace wbc2::ctrl;
 int main(int argc, char** argv)
 {
   std::cout << "Hello, c++ from test dir." << std::endl;
+  std::cout << "WBC2 Version : " << wbc2::getVersion() << std::endl;
 
   RobotPtr robot = std::make_shared<Robot>("sample_arm");
   robot->test();
